15165e96ae200f968d7e5f4be0877746bc7c7125
Pedro-Serer <42715517+Pedro-Serer@users.noreply.github.com>
2019-02-15 07:17:02 -0200

    Update Chat.php

--- Chat.php
+++ Chat.php
@@ -4,7 +4,7 @@
   if(!isset($_SESSION['nome'])){
     session_destroy();
     unset($_SESSION['nome']);
-    header("Location: http://127.0.0.1/Testes_Freela/login.php");
+    header("Location: http://127.0.0.1/login.php");
   }
 ?>
 
